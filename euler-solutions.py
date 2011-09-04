0debd82e73e6f45288561129a64f7fd276e952b8
kraihn <jared@dunesky.com>
2011-09-03 20:54:15 -0400

    Solution methods are called from the modules.

--- euler-solutions.py
+++ euler-solutions.py
@@ -6,12 +6,47 @@ import sys
 
 def main():
     quit = False
-    print 'Welcome to Euler Solutions\n'
+    print 'Welcome to Euler Solutions. This application and solutions were written by Jared Dickson.\n'
     while quit != True:
         print "Please enter the integer Id to view the description and the solution,\nor type 'q' to quit"
+        
         response = raw_input("Project Id: ")
+        print '\n'
+
         if response == 'q':
             quit = True
+
+        elif response.isdigit():
+            response = int(response)
+
+            if response > 0 and response < 100:                
+                try:
+                    method = getattr(p0xx, ('p' + str(response)))
+                    method()
+                except:
+                    print 'No solution available.'
+
+            elif response >= 100 and response < 200:                
+                try:
+                    method = getattr(p1xx, ('p' + str(response)))
+                    method()
+                except:
+                    print 'No solution available.'
+
+            elif response >= 200 and response < 300:                
+                try:
+                    method = getattr(p2xx, ('p' + str(response)))
+                    method()
+                except:
+                    print 'No solution available.'
+
+            elif response >= 300 and response < 400:                
+                try:
+                    method = getattr(p3xx, ('p' + str(response)))
+                    method()
+                except:
+                    print 'No solution available.'
+
         print '\n'
 
 
